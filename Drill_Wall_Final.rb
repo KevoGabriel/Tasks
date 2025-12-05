41f099a248b9c189ad765a415dbba6fd92dcacfa
Kevin PC 2 <kevinsehnem123@gmail.com>
2025-12-05 14:45:18 -0300

    Refatoração e eliminação de redundancia da criação de clone e sua respectiva instancia temporaria.

--- Drill_Wall_Final.rb
+++ Drill_Wall_Final.rb
@@ -3,56 +3,58 @@ def drill_wall(nome_parede, nome_cortador)
   model.start_operation('Clonar e Cortar Parede', true)
 
   begin
-    # Localiza componentes
-    parede = model.entities.grep(Sketchup::ComponentInstance).find { |c| c.definition.name == nome_parede }
-    cortador_orig = model.entities.grep(Sketchup::ComponentInstance).find { |c| c.definition.name == nome_cortador }
+    # Localiza os componentes pelo nome
+    parede  = model.entities.grep(Sketchup::ComponentInstance)
+                   .find { |c| c.definition.name == nome_parede }
 
-    unless parede && cortador_orig
+    cortador = model.entities.grep(Sketchup::ComponentInstance)
+                     .find { |c| c.definition.name == nome_cortador }
+
+    unless parede && cortador
       model.abort_operation
       return "Parede ou cortador não encontrados."
     end
 
-    # Cria clone retangular
-    bbox = cortador_orig.bounds
+    # Cria cortador temporário baseado no bounding box (ponto-chave)
+    bbox = cortador.bounds
     corners = (0..7).map { |i| bbox.corner(i) }
-
     base_corners = [corners[0], corners[1], corners[5], corners[4]]
 
-    clone_group = model.active_entities.add_group
-    face = clone_group.entities.add_face(base_corners)
+    temp_group = model.active_entities.add_group
+    face = temp_group.entities.add_face(base_corners)
     face.reverse! if face.normal.z < 0
-    clone_group.transform!(Geom::Transformation.translation([0, 0, 1.mm])) # Necessito mehorar isso para o Y ser variavel caso o cortador não esteja encostando na face da parede
 
-    cortador_clone = clone_group.to_component
-    cortador_clone.definition.name = "Clone_Retangular_#{cortador_orig.definition.name rescue 'SemNome'}"
+    # Necessita melhorar: se o cortador não estiver encostando na parede, o offset deveria ser variável
+    temp_group.transform!(Geom::Transformation.translation([0, 0, 1.mm]))
 
     # Configurações da parede
     entities_parede = parede.definition.entities
     trans_parede = parede.transformation
-    trans_clone = cortador_clone.transformation
 
     bounds_parede = parede.bounds
-    espessura = [bounds_parede.width, bounds_parede.height, bounds_parede.depth].min
+    espessura = [
+      bounds_parede.width,
+      bounds_parede.height,
+      bounds_parede.depth
+    ].min
 
     faces_antes = entities_parede.grep(Sketchup::Face)
 
-    # Instância temporária para interseção
-    temp = model.entities.add_instance(cortador_clone.definition, trans_clone)
-
+    # Interseção entre o cortador temporário e a parede (ponto-chave da operação)
     model.entities.intersect_with(
       true,
       Geom::Transformation.new,
       entities_parede,
       trans_parede,
       true,
-      temp
+      temp_group
     )
 
-    # PushPull nas novas faces
+    # PushPull apenas nas faces novas criadas pela interseção
     faces_depois = entities_parede.grep(Sketchup::Face)
-    novas = faces_depois - faces_antes
+    novas_faces = faces_depois - faces_antes
 
-    novas.each do |face|
+    novas_faces.each do |face|
       begin
         face.pushpull(-espessura)
       rescue => e
@@ -60,14 +62,12 @@ def drill_wall(nome_parede, nome_cortador)
       end
     end
 
-    # Limpeza
-    temp.erase! if temp.valid?
-    cortador_clone.erase! if cortador_clone.valid?
-    # Se quiser remover o cortador original (janela), descomente a linha abaixo
-    # cortador_orig.erase!
+    # Remove o cortador temporário
+    temp_group.erase! if temp_group.valid?
 
     model.commit_operation
     "Recorte concluído!"
+
   rescue => e
     model.abort_operation
     "Erro: #{e.message}"
